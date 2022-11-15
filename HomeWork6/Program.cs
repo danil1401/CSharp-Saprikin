89924404a2647a6118b9832ab773ef3eb8a9bb8b
danil1401 <danil1401@yandex.ru>
2022-11-15 23:26:03 +0400

    Завершение семинара 7 и решение ДЗ 6

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -9,6 +9,18 @@
 void Zadacha41()
 
 {
+Console.WriteLine("Сколько чисел нужно ввести? ");
+int countOfNumbers=Convert.ToInt32(Console.ReadLine());
+int count=0;//счетчик
+
+for(int i=1;i<=countOfNumbers;i++)
+{
+    Console.WriteLine($"Введите {i} число: ");
+    int inputNumber=Convert.ToInt32(Console.ReadLine());
+    
+    if(inputNumber>0) count++;
+}
+Console.WriteLine("Положительных чисел введено "+ count);
 
 }
 
@@ -17,7 +29,18 @@ void Zadacha41()
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
 void Zadacha43()
-
+//Решение через кортеж
+//использование не явной переменной var-автоматически поймет что нужно задать, число или строку
 {
-    
+    var(k1,b1)=(1,4);
+    var(k2,b2)=(5,-3);
+    Console.WriteLine(k1);
+    double x=(b2-b1)/(k1-k2);
+    x=Math.Round(x,2);//Math.Round (,2) округление числа до указанного знака после запятой
+    double y= k1*x+b1;
+    y=Math.Round(y,2);
+    Console.WriteLine($"Точка пересечения А ({x},{y})");
 }
+
+//Zadacha41();
+Zadacha43();
